fd47ba32ef8ec444a585254847e4b621d2223c1f
cesar jesus leon martin <cesilius98@gmail.com>
2022-05-26 12:15:13 +0200

    Fix: corregido nombre variables

--- src/com/company/VeredaGT.java
+++ src/com/company/VeredaGT.java
@@ -1,14 +1,15 @@
 package com.company;
 
 public class VeredaGT implements Vehicle {
-    private final int INITIALMOVEMENT=0;
+    private final int INITIAL_MOVEMENT = 0;
     private String matricula;
     private float maxEnergy;
     private float energy;
-    private int movementCount = INITIALMOVEMENT;
+    private int movementCount = INITIAL_MOVEMENT;
     private float energyDelta;
 
     public VeredaGT( String matricula, float maxEnergy, float energyDelta){
+
         this.energy=maxEnergy;
         this.maxEnergy=maxEnergy;
         this.matricula=matricula;
@@ -23,6 +24,7 @@ public class VeredaGT implements Vehicle {
 
     @Override
     public void recharge() {
+
         System.out.println("");
         System.out.println("---recargando---");
         System.out.println("");
